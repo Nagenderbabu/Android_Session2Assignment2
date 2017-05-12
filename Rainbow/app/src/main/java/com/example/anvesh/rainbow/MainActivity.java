791b2480c31f488a1f8edc0ad9c8f44b9cd33d9a
Nagenderbabu <nagenderbabu34@gmail.com>
2017-05-12 15:16:37 +0530

    RE Submitted with changes

--- Rainbow/app/src/main/java/com/example/anvesh/rainbow/MainActivity.java
+++ Rainbow/app/src/main/java/com/example/anvesh/rainbow/MainActivity.java
@@ -4,10 +4,9 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     // setting the layout to linear layout
-        setContentView(R.layout.linearlayout);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_main);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
